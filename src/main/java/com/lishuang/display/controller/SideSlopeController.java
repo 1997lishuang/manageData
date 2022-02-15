f9c55a5e62cb0def14971f8eb2224e16154b5b3c
lishuang <1320424353@qq.com>
2022-02-15 19:01:10 +0800

    完成隧洞模板功能 边坡变形监测初始化 建立边坡表

---
+++ src/main/java/com/lishuang/display/controller/SideSlopeController.java
@@ -0,0 +1,27 @@
+package com.lishuang.display.controller;
+
+
+import com.lishuang.display.commonutils.R;
+import org.springframework.stereotype.Controller;
+
+
+/*
+      边坡类
+ */
+@Controller
+public class SideSlopeController {
+
+
+
+
+    /*
+       surface deformation monitor
+      对边坡的变形监测
+     */
+    public R AddsurfaceMonitor(){
+
+
+        return R.ok();
+    }
+
+}
--- src/main/java/com/lishuang/display/controller/UploadController.java
+++ src/main/java/com/lishuang/display/controller/UploadController.java
@@ -3,6 +3,7 @@ package com.lishuang.display.controller;
 
 import cn.afterturn.easypoi.excel.ExcelImportUtil;
 import cn.afterturn.easypoi.excel.entity.ImportParams;
+import cn.afterturn.easypoi.word.WordExportUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.lishuang.display.commonutils.ExportWordUtil;
 import com.lishuang.display.commonutils.R;
@@ -10,8 +11,11 @@ import com.lishuang.display.mapper.RedcaveconvergenceMapper;
 import com.lishuang.display.model.ReadData;
 import com.lishuang.display.model.Redcaveconvergence;
 import com.lishuang.display.service.impl.RedcaveconvergenceServiceImpl;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,8 +27,9 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.swing.filechooser.FileSystemView;
-import java.io.File;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -46,6 +51,8 @@ public class UploadController {
      @Autowired
      public RedcaveconvergenceMapper redcaveconvergenceMapper;
 
+    @Autowired
+    public UploadController uploadController;
     @RequestMapping("/UpFile")
     public R UploadFile(@RequestParam(value = "file",required = false) MultipartFile multipartFile) throws Exception {
         ImportParams params = new ImportParams();
@@ -83,11 +90,161 @@ public class UploadController {
         return R.ok();
     }
 
-    @Autowired
-    public UploadController uploadController;
+
+    @RequestMapping("/UploadWord")
+    @ResponseBody
+    public R UploadWord(@RequestParam(value = "file",required = false) MultipartFile multipartFile)
+    {
+
+        if(multipartFile.isEmpty()){
+            //返回选择文件提示
+            return R.error();
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd/");
+        //构建文件上传所要保存的"文件夹路径"--这里是相对路径，保存到项目根路径的文件夹下
+        String realPath = new String("src/main/resources/templates");
+
+//        String format = sdf.format(new Date());
+        //存放上传文件的文件夹
+//        File file = new File(realPath + format);
+        File file = new File(realPath);
+
+        if(!file.isDirectory()){
+            //递归生成文件夹
+            file.mkdirs();
+        }
+        //获取原始的名字  original:最初的，起始的  方法是得到原来的文件名在客户机的文件系统名称
+        String oldName = multipartFile.getOriginalFilename();
+
+//        String newName = UUID.randomUUID().toString() + oldName.substring(oldName.lastIndexOf("."),oldName.length());
+
+        try {
+            //构建真实的文件路径
+            File newFile = new File(file.getAbsolutePath() + File.separator + oldName);
+            //转存文件到指定路径，如果文件名重复的话，将会覆盖掉之前的文件,这里是把文件上传到 “绝对路径”
+            multipartFile.transferTo(newFile);
+//            String filePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/uploadFile/" + format + newName;
+
+            return R.ok();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return R.ok();
+    }
 
 
 
+    /*
+       word模板下载
+     */
+    @RequestMapping("downWord")
+    @ResponseBody
+    @CrossOrigin
+    public R downWord(String templatesName) throws FileNotFoundException {
+
+
+        String substring = templatesName.substring(0, 4);
+
+        String eqName =  substring+ ".docx";
+
+        String path = ResourceUtils.getURL("classpath:").getPath();
+        System.out.println(path);
+
+        File file = null;
+        try {
+            file = ResourceUtils.getFile("classpath:templates");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        if(file.exists()){
+
+            File[] files = file.listFiles();
+            if(files != null){
+                for(File childFile:files){
+                    if(eqName.equals(childFile.getName())){
+
+
+                        //下载的文件路径
+                        String filePath = "src/main/resources/templates/"+eqName;
+                        // 开始下载文件
+                        InputStream in = null;
+                        try {
+                            // 1.设置文件ContentType类型，这样设置，会自动判断下载文件类型
+                            response.setContentType("multipart/form-data");
+                            // 2.设置文件头：最后一个参数是设置下载文件名
+
+                            response.addHeader("Content-Disposition",
+                                    "attachment;filename=" + new String("word模板.docx".getBytes(), "ISO-8859-1"));
+
+                            in = new FileInputStream(new File(filePath));
+
+                            // 3.通过response获取ServletOutputStream对象(out)
+                            int b = 0;
+                            byte[] buffer = new byte[512];
+                            while (b != -1) {
+                                b = in.read(buffer);
+                                if (b != -1) {
+                                    response.getOutputStream().write(buffer, 0, b);// 4.写到输出流(out)中
+                                }
+
+                            }
+                        } catch (Exception e) {
+                        } finally {
+                            try {
+                                if (in != null) {
+                                    in.close();
+                                }
+                                response.getOutputStream().flush();
+
+
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+
+
+
+
+//                        //下载的文件路径
+//                        String filePath = "src/main/resources/templates/"+eqName;
+//                        System.out.println(filePath);
+//                        //下载后显示的文件名
+//                        String fileName = "word模板文件";
+//                            //使用流的形式下载文件
+//                        try (
+//                                InputStream is = new FileInputStream(filePath);
+//                                OutputStream os = new BufferedOutputStream(response.getOutputStream());
+//                        ) {
+//                            byte[] buffer = new byte[is.available()];
+//                            is.read(buffer);
+//                            response.reset();
+//                            response.addHeader("Access-Control-Allow-Origin","*");
+//                            response.setContentType("application/octet-stream");
+//                            response.addHeader("Content-Disposition", "attachment;filename=" + new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1));
+//                            response.addHeader("Content-Length", "" + buffer.length);
+//                            os.write(buffer);
+//                            os.flush();
+//                            os.close();
+//                            is.close();
+//                            return null;
+//                        } catch (Exception e) {
+//                            e.printStackTrace();
+//                            return R.error();
+//                        }
+
+                    }
+
+                }
+            }
+        }
+
+
+        return null;
+
+    }
     /*
          循环导出 word 方法
      */
